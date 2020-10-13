abd18b7d3aa97971c55c866e73c3a3ccc1e6c856
Senendra <senendra.007@gmail.com>
2020-10-13 09:50:55 +0530

    [Senendra] Add: Creating game board and assigning empty values

---
+++ TicTakToeProgram/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace TicTakToeProgram
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            char[] gameBoard = CreateBoard();
+        }
+        public static char[] CreateBoard()
+        {
+            char[] gameBoard = new char[10];
+            for(int index = 0; index < 10; index++)
+            {
+                gameBoard[index] = ' ';
+            }
+            return gameBoard;
+        }
+    }
+}
