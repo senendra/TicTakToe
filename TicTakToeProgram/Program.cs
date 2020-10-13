4a475e4465f1271a7785f43ba43b57a01b5c6845
Senendra <senendra.007@gmail.com>
2020-10-13 10:37:21 +0530

    [Senendra] Refactor:Method for getting player letter choice and validating it

--- TicTakToeProgram/Program.cs
+++ TicTakToeProgram/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Security.Cryptography.X509Certificates;
+
 namespace TicTakToeProgram
 {
     class Program
@@ -6,15 +8,30 @@ namespace TicTakToeProgram
         public static void Main(string[] args)
         {
             char[] gameBoard = CreateBoard();
+            char playerLetter = ChooseLetter();
         }
         public static char[] CreateBoard()
         {
             char[] gameBoard = new char[10];
-            for(int index = 0; index < 10; index++)
+            for(int index = 1; index < gameBoard.Length ; index++)
             {
                 gameBoard[index] = ' ';
             }
             return gameBoard;
+        }
+        public static char ChooseLetter()
+        {
+            Console.WriteLine("Enter your choice (X or O) :");
+            string letter = Console.ReadLine();
+            if (char.ToUpper(letter[0]) == 'X'  || char.ToUpper(letter[0]) == 'O')
+            {
+            }
+            else
+            {
+                Console.WriteLine("You entered a invalid entry. ");
+                ChooseLetter();
+            }
+            return char.ToUpper(letter[0]);
         }
     }
 }
